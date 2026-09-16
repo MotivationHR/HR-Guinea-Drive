31e2569f7a17ff747ce32b262dcf99fdacbdc11d
MotivationHR <alamineagd224@gmail.com>
2026-09-16 22:40:03 +0200

    Add Unreal module build configuration

---
+++ Source/HRGuineaDrive/HRGuineaDrive.Build.cs
@@ -0,0 +1,24 @@
+using UnrealBuildTool;
+
+public class HRGuineaDrive : ModuleRules
+{
+    public HRGuineaDrive(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "InputCore",
+            "EnhancedInput",
+            "ChaosVehicles",
+            "UMG",
+            "Slate",
+            "SlateCore",
+            "Json",
+            "JsonUtilities"
+        });
+    }
+}
